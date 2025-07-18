de50686e9ae06ae8036f3c1ecd4953ac3e85dca1
ghazthiskc <ghazthiskc.ig@gmail.com>
2025-07-18 08:56:06 +0700

    Adding ball attack to player tank

--- Assets/Source/Scripts/BulletBehavior.cs
+++ Assets/Source/Scripts/BulletBehavior.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class BulletBehavior : MonoBehaviour
 {
-    void OnCollisionEnter(Collision other)
+    void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Arena"))
         {
@@ -10,12 +10,12 @@ public class BulletBehavior : MonoBehaviour
             Destroy(gameObject);
         }
     }
-    void OnTriggerEnter(Collider other)
+    void OnCollisionStay2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Arena"))
         {
-            Debug.Log("nabrak tembok 2");
+            Debug.Log("nabrak tembok 1");
             Destroy(gameObject);
         }
-    } 
+    }
 }
--- Assets/Source/Scripts/Player/PlayerAttack.cs
+++ Assets/Source/Scripts/Player/PlayerAttack.cs
@@ -4,7 +4,7 @@ using UnityEngine.InputSystem;
 
 public class PlayerAttack : MonoBehaviour
 {
-    public float delayAttack = 1f;
+    public float delayAttack = 3f;
     public float bulletSpeed = 5f;
     public GameObject bullet;
     public float time;
@@ -12,6 +12,26 @@ public class PlayerAttack : MonoBehaviour
     void Start()
     {
 
+    }
+    public void OnAttack(InputValue input)
+    {
+        if (!isAttacking)
+        {
+            StartCoroutine(DelayedAttack());
+        }
+    }
+    private IEnumerator DelayedAttack()
+    {
+        isAttacking = true;
+        LaunchAttack();
+        yield return new WaitForSeconds(delayAttack);
+        isAttacking = false;
+    }
+    private void LaunchAttack()
+    {
+        GameObject j = Instantiate(bullet, transform.position, transform.rotation);
+        Rigidbody2D rigidbody2D = j.GetComponent<Rigidbody2D>();
+        rigidbody2D.linearVelocity = bulletSpeed * transform.up;
     }
 
 }
